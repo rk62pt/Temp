284abc20c2571f8d6de0f910c8e980c95cb9fe2b
rk62pt <rk62pt@gmail.com>
2016-04-06 17:14:32 +0800

    Create HomeVO.java

---
+++ HomeVO.java
@@ -0,0 +1,26 @@
+package com.test.home.vo;
+
+public class HomeVO {
+	private String message;
+	private String status;
+	private String body;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+}
