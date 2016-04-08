c400b2d614e40a2f6c24d9f58b4d0cf4c4f7604e
rk62pt <rk62pt@gmail.com>
2016-04-08 09:44:23 +0800

    Update HomeApiController.java

--- HomeApiController.java
+++ HomeApiController.java
@@ -1,6 +1,8 @@
 package com.test.home;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,29 +11,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.test.home.vo.HomeVO;
+import com.test.home.vo.ItemVO;
+import com.test.home.vo.ResultVO;
 
 @Controller
 @RequestMapping("/home")
 public class HomeApiController {
 	@RequestMapping(value = "getTitle",method = RequestMethod.GET)
-	public @ResponseBody HomeVO getTitle(){
-		HomeVO home = new HomeVO();
-		home.setMessage("成功");
-		home.setStatus("success");
-		home.setBody("AJAX is a");
-		return home;
+	public @ResponseBody ResultVO<String> getTitle(){
+		ResultVO<String> vo = new ResultVO<String>();
+		vo.setMessage("成功");
+		vo.setStatus("success");
+		vo.setBody("this is t");
+		return vo;
 	}
 	
 	@RequestMapping(value = "getList/{cnt}",method = RequestMethod.GET)
-	public @ResponseBody HomeVO getList(@PathVariable int cnt){
-		HomeVO home = new HomeVO();
-		String[] _cntList = new String[cnt];
-		for(int i = 0 ; i< _cntList.length;i++){
-			_cntList[i] = "\"test"+i+"\"";
+	public @ResponseBody ResultVO<List<String>> getList(@PathVariable int cnt){
+		ResultVO<List<String>> home = new ResultVO<List<String>>();
+		
+		List<String> _cntList = new ArrayList<String>();
+		for(int i = 0 ; i< cnt;i++){
+			_cntList.add("test"+i);
 		}
 		home.setMessage("成功");
 		home.setStatus("success");
-		home.setBody(Arrays.toString(_cntList));
+		home.setBody(_cntList);
 		return home;
 	}
 }
